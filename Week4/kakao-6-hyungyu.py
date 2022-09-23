e738263410fb4a4342c86493ebb35c772abb4030
HyunG1u <shgusrb0402@naver.com>
2022-09-23 15:27:06 +0900

    신고 결과 받기

--- Week4/kakao-6-hyungyu.py
+++ Week4/kakao-6-hyungyu.py
@@ -1,11 +1,12 @@
 def solution(id_list, report, k):
     answer = [0] * len(id_list)
     dicReport={x:0 for x in id_list}
+    report=set(report)
     
-    for r in set(report):
+    for r in report:
         dicReport[r.split()[1]] +=1
         
-    for r in set(report):
+    for r in report:
         if dicReport[r.split()[1]] >=k:
             answer[id_list.index(r.split()[0])]+=1
     
