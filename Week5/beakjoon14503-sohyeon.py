7c5af19dfc5d09cd6e73416d1629bdff9a6c1797
thguss <thgus345@naver.com>
2022-09-27 02:00:43 +0900

    [구글링] 로봇 청소기 - 클린 코드

--- Week5/beakjoon14503-sohyeon.py
+++ Week5/beakjoon14503-sohyeon.py
@@ -8,11 +8,7 @@ dest = [[-1,0], [0,1], [1,0], [0,-1]]
 visited = [[0]*m for _ in range(n)]
 visited[r][c] = 1
 
-cnt = 1
-
-while True:
-  flag = 0
-
+def fourSearching(r, c, d):
   for _ in range(4):
     dx, dy = r + dest[(d+3)%4][0], c + dest[(d+3)%4][1]
 
@@ -20,18 +16,29 @@ while True:
       visited[dx][dy] = 1
       d = (d+3)%4
       r, c = dx, dy
-      cnt += 1
-      flag = 1
-      break
-
+      return r, c, d
+    
     d = (d+3)%4
+  
+  return False
 
-  if not flag:
+cnt = 1
+
+while True:
+  res = fourSearching(r, c, d)
+
+  if not res:
     bx, by = r - dest[d][0], c - dest[d][1]
     if 0 <= bx < n and 0 <= by < m and not board[bx][by]:
       r, c = bx, by
     else:
       break
+  
+  else:
+    r, c, d = res
 
 
-print(cnt)
+ans = 0
+for v in visited:
+  ans += v.count(1)
+print(ans)
