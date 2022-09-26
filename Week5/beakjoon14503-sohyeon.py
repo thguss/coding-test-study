1ef8babb43b57bc572a2516ba1a0a2c467dfede7
thguss <thgus345@naver.com>
2022-09-27 01:55:02 +0900

    [구글링] 로봇 청소기

--- Week5/beakjoon14503-sohyeon.py
+++ Week5/beakjoon14503-sohyeon.py
@@ -1,4 +1,3 @@
-from collections import deque
 
 n, m = map(int, input().split())
 r, c, d = map(int, input().split())
@@ -6,37 +5,33 @@ board = [list(map(int, input().split())) for _ in range(n)]
 
 dest = [[-1,0], [0,1], [1,0], [0,-1]]
 
-queue = deque()
-queue.append([r, c, d])
-
 visited = [[0]*m for _ in range(n)]
 visited[r][c] = 1
 
-def searching(x, y, w):
-  for t in range(1, 5):
-    dx, dy = x + dest[(w-t)%4][0], y + dest[(w-t)%4][1]
+cnt = 1
+
+while True:
+  flag = 0
+
+  for _ in range(4):
+    dx, dy = r + dest[(d+3)%4][0], c + dest[(d+3)%4][1]
 
     if 0 <= dx < n and 0 <= dy < m and not board[dx][dy] and not visited[dx][dy]:
       visited[dx][dy] = 1
-      queue.append([dx, dy, w-1])
-      return True
+      d = (d+3)%4
+      r, c = dx, dy
+      cnt += 1
+      flag = 1
+      break
 
-  return False
+    d = (d+3)%4
 
-while queue:
-  x, y, w = queue.popleft()
-
-  if not searching(x, y, w):
-    dx, dy = x - dest[w%4][0], y - dest[w%4][1]
-
-    if 0<= dx < n and 0<= dy < m and not board[dx][dy]:
-      queue.append([dx, dy, w])
-
-
-answer = 0
-for v in visited:
-  answer += v.count(1)
-
-print(answer)
+  if not flag:
+    bx, by = r - dest[d][0], c - dest[d][1]
+    if 0 <= bx < n and 0 <= by < m and not board[bx][by]:
+      r, c = bx, by
+    else:
+      break
 
 
+print(cnt)
