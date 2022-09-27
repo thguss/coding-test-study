cd4e693936266adf4555482acf590e88cf2208a8
ym-byun <tyg04308@naver.com>
2022-09-27 16:35:46 +0900

    [코딩테스트 공부] - 문제 해결 조건 추가

--- Week5/kakao3-youngmu.py
+++ Week5/kakao3-youngmu.py
@@ -1,5 +1,5 @@
 def solution(alp, cop, problems):
-    dp = [[987654321 for _ in range(150)] for _ in range(150)]
+    dp = [[987654321 for _ in range(152)] for _ in range(152)]
     dp[alp][cop] = 0
     goal_alp = 0
     goal_cop = 0
@@ -19,7 +19,7 @@ def solution(alp, cop, problems):
                 dp[a + 1][c] = min(dp[a + 1][c], dp[a][c] + 1)
             else:
                 dp[goal_alp][c] = min(dp[goal_alp][c], dp[a][c] + 1)
-                
+
             # Case 2. 코딩력 1 증가.
             if c + 1 <= goal_cop:
                 dp[a][c + 1] = min(dp[a][c + 1], dp[a][c] + 1)
@@ -29,19 +29,20 @@ def solution(alp, cop, problems):
             # Case 3. 문제 풀기
             for problem in problems:
                 alp_req, cop_req, alp_rwd, cop_rwd, cost = problem
-                if a + alp_rwd <= goal_alp and c + cop_rwd <= goal_cop:
-                    dp[a + alp_rwd][c + cop_rwd] = min(dp[a + alp_rwd][c + cop_rwd], dp[a][c] + cost)
+                if a >= alp_req and c >= cop_req:
+                    if a + alp_rwd <= goal_alp and c + cop_rwd <= goal_cop:
+                        dp[a + alp_rwd][c + cop_rwd] = min(dp[a + alp_rwd][c + cop_rwd], dp[a][c] + cost)
 
-                elif a + alp_rwd > goal_alp and c + cop_rwd > goal_cop:
-                    dp[goal_alp][goal_cop] = min(dp[goal_alp][goal_cop], dp[a][c] + cost)
+                    elif a + alp_rwd > goal_alp and c + cop_rwd > goal_cop:
+                        dp[goal_alp][goal_cop] = min(dp[goal_alp][goal_cop], dp[a][c] + cost)
 
-                else:
-                    if a + alp_rwd > goal_alp:
-                        dp[goal_alp][c + cop_rwd] = min(dp[goal_alp][c + cop_rwd], dp[a][c] + cost)
                     else:
-                        dp[a + alp_rwd][goal_cop] = min(dp[a + alp_rwd][goal_cop], dp[a][c] + cost)
+                        if a + alp_rwd > goal_alp:
+                            dp[goal_alp][c + cop_rwd] = min(dp[goal_alp][c + cop_rwd], dp[a][c] + cost)
+                        else:
+                            dp[a + alp_rwd][goal_cop] = min(dp[a + alp_rwd][goal_cop], dp[a][c] + cost)
 
     return dp[goal_alp][goal_cop]
 
 
-print(solution(10, 10, [[10,15,2,1,2],[20,20,3,3,4]]))
+print(solution(10, 10, [[10, 15, 2, 1, 2], [20, 20, 3, 3, 4]]))
